b74b76cb33e6dc98d63e9aa5cf8d2bb99a8d0344
PjHurtig <>
2023-07-20 16:32:30 +0000

    add style classes to card image to maintain image aspect ratio

--- event/views.py
+++ event/views.py
@@ -5,6 +5,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1-3).order_by('event_date')
+    queryset = Post.objects.filter(status=1).order_by('event_date')
     template_name = 'index.html'
     paginate_by = 6
