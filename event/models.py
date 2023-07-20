49cb8f6ade0b4029cd56da1e66f6162837abc65d
PjHurtig <>
2023-07-20 13:05:01 +0000

    add post model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
-# Create your models here.
+EVENT_TYPES = ((0, 'Music'), (1, 'Art'), (2, 'Other'))
+STATUS = ((0, 'Draft'), (1, 'Published'), (2, 'Done'), (3, 'Cancelled'))
+AREA_CHOICES = [
+    ('jamtland', 'Jämtland'),
+    ('ostersund', 'Östersund'),
+    ('krokom', 'Krokom'),
+    ('berg', 'Berg'),
+    ('are', 'Åre'),
+    ('bracke', 'Bräcke'),
+    ('harjedalen', 'Härjedalen'),
+    ('ragunda', 'Ragunda'),
+    ('stromsund', 'Strömsund'),
+]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    event_date = models.DateField(default=timezone.now)
+    event_time = models.TimeField(default=timezone.now)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="event_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    about = models.TextField()
+    event_image = CloudinaryField('image', default='placeholder')
+    link = models.URLField(blank=True)
+    area = models.CharField(max_length=100, choices=AREA_CHOICES,
+                            default='jamtland')
+    locale = models.CharField(max_length=100)
+    event_type = models.IntegerField(choices=EVENT_TYPES, default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
