74610bb1d7e008db953ec88f55f8c98dd3b85900
PjHurtig <>
2023-07-20 13:07:05 +0000

    add post admin class to admin.py to generate slug and display list in admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Post
+from django.utils.text import slugify
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('event_date', 'event_time', 'artist',)
+    }
+    list_filter = ('status', 'created_on', 'updated_on', 'event_date', 'area')
+    list_display = ('status', 'created_on', 'updated_on', 'event_date', 'area')
+    search_fields = ['about', 'title', 'artist']
